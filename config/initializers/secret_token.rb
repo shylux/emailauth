e2ac325a920f9cc0207c33a7f6df7400d0bee004
Lukas Knoepfel <shylux@gmail.com>
2014-10-02 17:24:21 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fantasyraid::Application.config.secret_token = '9594df74db6263dc68849311c0c2bbc3dc79aa2601d552b0d10dcaf20b07456c48b5e89df748745bb79c341097b33eedfb1cc4d7196586058ab2f0d1194db55d'
