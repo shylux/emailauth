4bba57f1702cb81c3d68085740a14460f1e49295
Lukas Knoepfel <shylux@gmail.com>
2014-10-16 17:13:04 +0200

    Added email confirmation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save { self.email = email.downcase }
+
+  attr_accessible :email, :name, :email_confirmation
   has_many :auth_sessions
-  validates :name, length: { maximum: 20 }
+  validates :name, length: { in: 3..20 }
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX },
+                    confirmation: true
+  validates_presence_of :email_confirmation
 end
